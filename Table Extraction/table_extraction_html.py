2e8d3246c896378f6a8e6aa467f033158ce7c59b
hayden <hemc7@sbcglobal.net>
2025-03-19 17:51:15 -0500

    completed table extraction from simpsons wiki

---
+++ Table Extraction/table_extraction_html.py
@@ -0,0 +1,15 @@
+"""
+Simple program that imports pandas, reads an html file (in this case, it is a wikipedia page that 
+includes a table of all the simpsons episodes and other information), and prints out the table for season 1 of the simpsons.
+
+Note that there must be <table></table> html tag(s) included in the html file in order for pandas to successfully recognize
+and read the table. 
+"""
+
+import pandas as pd 
+
+simpsons = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
+
+print(len(simpsons))
+
+print(f"Table 1: \n\n{simpsons[1]}")
