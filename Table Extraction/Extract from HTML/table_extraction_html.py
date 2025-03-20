8d75e8def2e0cd53f333a6e93abb7caa7e71f2fa
hayden <hemc7@sbcglobal.net>
2025-03-20 16:28:33 -0500

    added more comments

--- Table Extraction/Extract from HTML/table_extraction_html.py
+++ Table Extraction/Extract from HTML/table_extraction_html.py
@@ -10,6 +10,6 @@ import pandas as pd
 
 simpsons = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
 
-print(len(simpsons))
+print(len(simpsons))        #prints 24, since there are 24 tables that were found at the HTML link provided.
 
-print(f"Table 1: \n\n{simpsons[1]}")
+print(f"Season 1: \n\n{simpsons[1]}")
