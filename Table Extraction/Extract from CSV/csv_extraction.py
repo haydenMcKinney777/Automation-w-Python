ab84a465790415483707d887a3615865f05db6a2
hayden <hemc7@sbcglobal.net>
2025-03-21 16:36:58 -0500

    deleted temporary csv file and edited comments

--- Table Extraction/Extract from CSV/csv_extraction.py
+++ Table Extraction/Extract from CSV/csv_extraction.py
@@ -1,5 +1,5 @@
 """
-Simple program that extracts a csv file found from a URL using only pandas. csv files are found at the website https://www.football-data.co.uk/data.php within this website
+Simple program that extracts a csv file found from a URL using only pandas. csv files are found at the website https://www.football-data.co.uk/data.php and within this website
 we are accessing the japan league.
 """
 
@@ -7,7 +7,7 @@ import pandas as pd
 
 df_japan = pd.read_csv("https://www.football-data.co.uk/new/JPN.csv") #URL's are not the only thing that you can insert as a parameter - see https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html for a full list of parameters
 
-#if you were to print df_japan directly above this comment, you would see the dataframe table output in the terminal window. However you might see some terms in the columns
+#if you were to print df_japan, you would see the dataframe table output in the terminal window. You might see some terms in the columns
 # that you are unfamiliar with, such as 'HG' or 'AG' etc. below, we will change some of the column names in case you do not know what they mean (note we changed 'HG' and 'AG' to home_goals and away_goals):
 
 df_japan.rename(columns={'HG' : "home_goals",
